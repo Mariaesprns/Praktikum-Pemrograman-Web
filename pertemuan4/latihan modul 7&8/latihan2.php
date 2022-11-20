456e89ac98cefc2fb790352c3cf6d64b33d7ab81
Mariaesprns <95108359+Mariaesprns@users.noreply.github.com>
2022-11-20 20:42:29 +0700

    Add files via upload

---
+++ pertemuan4/latihan modul 7&8/latihan2.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Pemrograman PHP dengan Array</title>
+</head>
+<body>
+    <?php
+    $nama[] = "Maria";
+    $nama[] = "Esperansa";
+    echo $nama[0] . $nama[1];
+    echo "<br>";
+    $jum_array = count($nama);
+echo "jumlah elemen array = ". $jum_array;
+    ?>
+    </body>
+    </html>
